84a8ed2ca5da4fbdb510d6da50c903f04a27eaf9
pratikmulle903 <pratikmulle903@gmail.com>
2021-07-14 17:54:59 +0530

    updated settings

--- tubers/settings.py
+++ tubers/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sxu8twuori6jfpxj#h%$8bk(w573-(#!5wb^h+xsy$^0t-%mps'
+# SECRET_KEY = 'django-insecure-sxu8twuori6jfpxj#h%$8bk(w573-(#!5wb^h+xsy$^0t-%mps'
+SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotubers.herokuapp.com']
 
 
 # Application definition
